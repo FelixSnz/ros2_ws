9bc80b4174270d663ea5789610023c6d9fea5735
FelixSnz <felixsanchez0124@gmail.com>
2021-02-07 14:49:08 -0600

    building of turtlesim package

---
+++ build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/felix/ros2_dev_ws/build/turtlesim/turtlesim__py
+# Build directory: /home/felix/ros2_dev_ws/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
